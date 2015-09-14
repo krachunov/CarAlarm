41a0498b8ccebfe6e4c71dd6d626507c9eb5d7aa
Krachunov <Krachunov@Krachunov-PC>
2015-09-14 16:47:19 +0300

    Clean main method

--- CarAlarm/src/Demo.java
+++ CarAlarm/src/Demo.java
@@ -20,29 +20,28 @@ public class Demo {
 			IOException, InvalidCarDataException, ParseException, DuplicateName {
 
 		OfficeProgram testProgram = new OfficeProgram("CU");
+
 		testProgram.addAgent("Perfekt", 8);
 		testProgram.addAgent("plovdiv", 9);
-		MyAgent agentPerfect = testProgram.getAgents().get(8);
-		MyAgent agentPerfect2 = testProgram.getAgents().get(9);
+		MyAgent agent1 = testProgram.getAgents().get(8);
+		MyAgent agent2 = testProgram.getAgents().get(9);
 
-		agentPerfect.crateNewCar("Ca80785TX", "307", "peugeot", "sdf789", 1.2d,
+		agent1.crateNewCar("Ca80785TX", "307", "peugeot", "sdf789", 1.2d,
 				"01/01/2005", 5, 90);
-		agentPerfect.cratenewClient(951147125L, "hristo", "krachunov",
-				"0899996347");
-		agentPerfect.createPolicy(8511147125l, "Ca8078TX", 2216612145145l,
+		agent1.cratenewClient(951147125L, "hristo", "krachunov", "0899996347");
+		agent1.createPolicy(8511147125l, "Ca8078TX", 2216612145145l,
 				"14/09/2015", "14/09/2015");
 
-		agentPerfect2.crateNewCar("Ca8078TX", "307", "peugeot", "sdf789", 1.2d,
+		agent2.crateNewCar("Ca8078TX", "307", "peugeot", "sdf789", 1.2d,
 				"01/01/2005", 5, 90);
-		agentPerfect2.cratenewClient(8511147125L, "hristo", "krachunov",
-				"0899996347");
-		agentPerfect2.createPolicy(8511147125l, "Ca8078TX", 22112145145l,
+		agent2.cratenewClient(8511147125L, "hristo", "krachunov", "0899996347");
+		agent2.createPolicy(8511147125l, "Ca8078TX", 22112145145l,
 				"14/09/2015", "14/09/2015");
 
 		System.out.println(OfficeProgram.getPeople().size());
 		System.out.println(OfficeProgram.getCars().size());
 		System.out.println(OfficeProgram.getPolicies().size());
-		System.out.println("Agent1 " + agentPerfect.getPolicy().size());
-		System.out.println("Agent2 " + agentPerfect2.getPolicy().size());
+		System.out.println("Agent1 " + agent2.getPolicy().size());
+		System.out.println("Agent2 " + agent2.getPolicy().size());
 	}
 }
